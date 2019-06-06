db9afc0d885baa7ce7a55702dad34dfe3d6a0f99
helloforrestworld@163.com <helloforrestworld@163.com>
2019-06-06 22:07:08 +0800

    feat((add example)):

--- examples/server.js
+++ examples/server.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const WebpackConfig = require('./webpack.config')
 
 const app = express()
 const router = express.Router()
+const compiler = webpack(WebpackConfig)
+
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: `hello world`
+  })
+})
+
+app.use(router)
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
+app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(router)
-
-router.get('/base/get', function(req, res) {
-  res.json(req.query)
-})
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,11 @@
 import { AxiosRequestConfig } from './types'
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, method = 'get', url } = config
+export default function xhr(config: AxiosRequestConfig) {
+  const { data = {}, method = 'get', url } = config
 
   const request = new XMLHttpRequest()
 
-  request.open(method.toUpperCase(), url, true)
+  request.open(method.toLowerCase(), url, true)
 
   request.send(data)
 }
