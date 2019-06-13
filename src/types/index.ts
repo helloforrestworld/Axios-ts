e70c2fbfdfeb70b9497ad1330d7510793ae32018
helloforrestworld@163.com <helloforrestworld@163.com>
2019-06-13 21:15:17 +0800

    axios 方法扩展

--- src/types/index.ts
+++ src/types/index.ts
@@ -26,6 +26,8 @@ export interface Axios {
 }
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
 export interface AxiosRequestConfig {
   url?: string
